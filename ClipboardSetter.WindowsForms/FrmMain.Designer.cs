b9be4b2f8cf87adddeec740a6b280bd1edc70b7c
warnov <warnov@gmail.com>
2020-05-16 01:03:58 -0500

    first commit

---
+++ ClipboardSetter.WindowsForms/FrmMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ClipboardSetter.WindowsForms
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtClipboardContent = new System.Windows.Forms.TextBox();
+            this.BtnNext = new System.Windows.Forms.Button();
+            this.BtnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtClipboardContent
+            // 
+            this.txtClipboardContent.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClipboardContent.Location = new System.Drawing.Point(90, 41);
+            this.txtClipboardContent.Name = "txtClipboardContent";
+            this.txtClipboardContent.Size = new System.Drawing.Size(111, 80);
+            this.txtClipboardContent.TabIndex = 0;
+            // 
+            // BtnNext
+            // 
+            this.BtnNext.Location = new System.Drawing.Point(12, 155);
+            this.BtnNext.Name = "BtnNext";
+            this.BtnNext.Size = new System.Drawing.Size(85, 23);
+            this.BtnNext.TabIndex = 1;
+            this.BtnNext.Text = "&Next";
+            this.BtnNext.UseVisualStyleBackColor = true;
+            this.BtnNext.Click += new System.EventHandler(this.BtnNext_Click);
+            // 
+            // BtnRestart
+            // 
+            this.BtnRestart.Location = new System.Drawing.Point(201, 155);
+            this.BtnRestart.Name = "BtnRestart";
+            this.BtnRestart.Size = new System.Drawing.Size(75, 23);
+            this.BtnRestart.TabIndex = 3;
+            this.BtnRestart.Text = "&Restart";
+            this.BtnRestart.UseVisualStyleBackColor = true;
+            this.BtnRestart.Click += new System.EventHandler(this.BtnRestart_Click);
+            // 
+            // FrmMain
+            // 
+            this.AcceptButton = this.BtnNext;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(288, 199);
+            this.Controls.Add(this.BtnRestart);
+            this.Controls.Add(this.BtnNext);
+            this.Controls.Add(this.txtClipboardContent);
+            this.Name = "FrmMain";
+            this.Text = "ClipboardSetter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtClipboardContent;
+        private System.Windows.Forms.Button BtnNext;
+        private System.Windows.Forms.Button BtnRestart;
+    }
+}
+
---
+++ ClipboardSetter.WindowsForms/FrmMain.cs
@@ -0,0 +1,56 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Windows.Forms;
+
+namespace ClipboardSetter.WindowsForms
+{
+    public partial class FrmMain : Form
+    {
+        int idx = 0;
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            Subscribe();
+        }
+
+
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            idx++;
+            txtClipboardContent.SelectAll();
+            txtClipboardContent.Text = idx.ToString();
+            Clipboard.SetText(txtClipboardContent.Text);
+        }
+        public void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.KeyUp += GlobalHook_KeyUp; 
+        }
+
+        private void ShowForm()
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;          
+            txtClipboardContent.Focus();
+            txtClipboardContent.SelectAll();
+        }
+
+        private void GlobalHook_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Add && e.Shift && e.Control)
+            {
+                ShowForm();
+                Activate();
+            }
+        }
+
+        private void BtnRestart_Click(object sender, EventArgs e)
+        {
+            idx = 0;
+        }
+    }
+}
